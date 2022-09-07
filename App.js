36f7dc0e44f84fbaf4122d503acb8f8c2284820e
Irvan-Hardyanto <hardyanto.irvan@gmail.com>
2022-09-07 09:27:36 +0700

    Install modul nodemon untuk otomatis run script ketika diubah

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 //console.log('hello world!')
 
 const validator = require('validator');
-console.log(validator.isEmail('harydanto.irvangmail.com'));
+console.log(validator.isEmail('harydanto.irvan@gmail.com'));
+console.log(validator.isMobilePhone('082219079290','id-ID'));
